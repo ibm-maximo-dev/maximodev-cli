ff0a62d1972cb002eaa612309b89ca1e5d4ca12c
Sean Stuckless <sls@ca.ibm.com>
2018-04-25 09:07:35 -0400

    hide commands that are in beta

--- src/maximodev-cli-create.js
+++ src/maximodev-cli-create.js
@@ -8,20 +8,26 @@
 
 var program = require('commander');
 
-program
+var prog = program
   .version('0.0.1')
   .description('Create a maximo artifact')
   .command('addon', 'create a maximo add-on').alias('product')
   .command('dbc-script', 'create an empty dbc in the product\'s script directory').alias('dbc')
   .command('product-xml', 'create product xml file in products directory').alias('prod')
   .command('presentation-diff', 'compare two presentation xml files and create a diff dbc script').alias('pd')
-  .command('java-field-validator', 'create Java field validation class').alias('jfv')
-  //.command('script-field-validator', 'create Script field validation').alias('sfv')
   .command('sample-classic-app', 'create a sample maximo classic application').alias('sca')
-  .command('classic-miniapp', 'Creates a MiniApp for use in the Classic UI').alias('miniapp')
-  .command('java-mbo', 'create a java MBO (Maximo Business Object) structure for Maximo development ').alias('mbo')
+  .command('java-mbo', 'create a java MBO (Maximo Business Object) structure for Maximo development ').alias('mbo');
+
+if (process.env["MAXIMODEV_CLI_BETA"]) {
+  prog.command('java-field-validator', 'create Java field validation class').alias('jfv');
+  prog.command('script-field-validator', 'create Script field validation').alias('sfv');
+  prog.command('classic-miniapp', 'Creates a MiniApp for use in the Classic UI').alias('miniapp')
   //.command('sample-nextgenui-app', 'create a sample maximo nextgenui application').alias('snga')
-  .parse(process.argv);
+}
+
+prog.parse(process.argv);
+
+
 
 // check if the user passed a command
 if (!program.commands.map(cmd => cmd._name).includes(program.args[0])) {
--- src/maximodev-cli.js
+++ src/maximodev-cli.js
@@ -9,24 +9,30 @@
 // https://www.npmjs.com/package/commander
 var program = require('commander');
 
-program
+var prog = program
   .version('0.0.1')
   .description('Maximo command line tools')
   .command('create [options]', 'create a maximo artifact, such as mbo, add-on, etc').alias('cr')
-  .command('docker [options]', 'run a commands against Docker images/containers').alias('dk')
   .command('build', 'build the current addon').alias('b')
-  // .command('deploy', 'deploy local maximo artifacts to a dev server').alias('d')
-  // .command('package', 'package an add-on for distribution').alias('d')
   .command('init [options]', 'initialize maximo add-on environment and java').alias('i')
   .command('update [options]', 'update a maximo artifact such as product xml version').alias('u')
   .command('set [options]', 'set add-on option').alias('s')
   .command('run-dbc [options]', 'run a dbc script file').alias('r')
-  // .command('export [options]', 'export something from a maximo instance').alias('exp')
-  // .command('import [options]', 'import something into a maximo instance').alias('imp')
-  // .command('clone [options]', 'clone a maximo application').alias('cl')
-  .command('deploy', 'publish a build to an artifact server').alias('d')
-  //.command('test', 'run all tests for the add-on').alias('d')
-  .parse(process.argv);
+  .command('deploy', 'publish a build to an artifact server').alias('d');
+
+if (process.env["MAXIMODEV_CLI_BETA"]) {
+  prog.command('docker [options]', 'run a commands against Docker images/containers').alias('dk');
+}
+
+// .command('deploy', 'deploy local maximo artifacts to a dev server').alias('d')
+// .command('package', 'package an add-on for distribution').alias('d')
+// .command('export [options]', 'export something from a maximo instance').alias('exp')
+// .command('import [options]', 'import something into a maximo instance').alias('imp')
+// .command('clone [options]', 'clone a maximo application').alias('cl')
+//.command('test', 'run all tests for the add-on').alias('d')
+
+prog.parse(process.argv);
+
 
 // check if the user passed a command
 if (!program.commands.map(cmd => cmd._name).includes(program.args[0])) {
